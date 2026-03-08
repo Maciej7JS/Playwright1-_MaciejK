4518917167aa3be845906d37290f14ab9378d82c
Maciej7JS <macieysee27@gmail.com>
2026-03-08 22:24:32 +0100

    Update playground.spec.js

--- playground.spec.js
+++ playground.spec.js
@@ -5,18 +5,17 @@ import { Playground } from '../pages/playground';
 
 test.beforeEach(async ({ page }) => {
   const playground = new Playground(page);
-
   await playground.navigateTo();
 });
 
 
-test('Button section - button displays correct status after clicking', async ({ page }) => {
-  const playground = new Playground(page);
+// test('Button section - button displays correct status after clicking', async ({ page }) => {
+//   const playground = new Playground(page);
 
-  await playground.clickButtonInGivenState(true);
-  await playground.clickButtonInGivenState(false);
+//   await playground.clickButtonInGivenState(true);
+//   await playground.clickButtonInGivenState(false);
 
-});
+// });
 
 
 test('Button with timer - single click', async ({ page }) => {
@@ -25,36 +24,24 @@ test('Button with timer - single click', async ({ page }) => {
 
   const buttonLoadTimeout = 7000; // button can be updated in beetween 3 and 7 seconds
 
-await page.goto('https://mad-qa.pl/playground/');
-await expect(page.getByTestId('timer-btn')).toBeVisible();
-  await expect(page.getByRole('heading', { name: 'Timer Button' })).toBeVisible();
+await expect(page.getByRole('heading', { name: 'Timer Button' })).toBeVisible();
 
-
-  await expect(page.getByTestId('timer-result')).toContainText('Waiting for click');
-
-  await page.getByTestId('timer-btn').click();
-  await expect(page.getByTestId('timer-result')).toHaveText('Processing...');
-
-  await expect(page.getByTestId('timer-result')).toHaveText('Complete', { timeout: buttonLoadTimeout });
+  await playground.clickTimerButton(buttonLoadTimeout);
 
 });
 
+
 test('Button with timer - multiple click', async ({ page }) => {
 
+  const playground = new Playground(page);
   const buttonLoadTimeout = 7000; // button can be updated in beetween 3 and 7 seconds
 
   await expect(page.getByRole('heading', { name: 'Timer Button' })).toBeVisible();
 
+  await playground.clickTimerButton(buttonLoadTimeout);
+  await playground.clickTimerButton(buttonLoadTimeout);
 
-  await expect(page.getByTestId('timer-result')).toContainText('Waiting for click');
-
-  await page.getByTestId('timer-btn').click();
-  await expect(page.getByTestId('timer-result')).toHaveText('Processing...');
-
-  await expect(page.getByTestId('timer-result')).toHaveText('Complete', { timeout: buttonLoadTimeout });
-
-  await page.getByTestId('timer-btn').click();
-  await expect(page.getByTestId('timer-result')).toHaveText('Processing...');
+});
 
   await expect(page.getByTestId('timer-result')).toHaveText('Complete', { timeout: buttonLoadTimeout });
 
